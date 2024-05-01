faa7340dbaeeed3cacdd88e725229f8d69227869
tarumi <taryunelab@gmail.com>
2024-05-01 18:55:21 +0200

    init

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-contract Contract { }
+contract Contract {
+    function onchain() public {}
+
+    function offchain() public pure returns (bytes memory) {
+        /* This is where you will write all your compute-heavy,
+        off-chain logic to be done in a Kettle */
+        return abi.encodeWithSelector(this.onchain.selector);
+    }
+}
